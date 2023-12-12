a185a54d009a38d06c06bccafa6cb5dff389d72b
Chad Lengefeld <chadlengefeld@chads-imac.lan>
2023-12-12 17:04:40 -0600

    attempted deploy fix

--- types/index.ts
+++ types/index.ts
@@ -1,24 +1,24 @@
-import { MouseEventHandler } from "react";
+// import { MouseEventHandler } from "react";
 
-export interface CustomButtonProps {
-    isDisabled?: boolean;
-    btnType?: "button" | "submit";
-    containerStyles?: string;
-    textStyles?: string;
-    title: string;
-    rightIcon?: string;
-    handleClick?: MouseEventHandler<HTMLButtonElement>;
-  }
+// export interface CustomButtonProps {
+//     isDisabled?: boolean;
+//     btnType?: "button" | "submit";
+//     containerStyles?: string;
+//     textStyles?: string;
+//     title: string;
+//     rightIcon?: string;
+//     handleClick?: MouseEventHandler<HTMLButtonElement>;
+//   }
 
-  export interface HomeProps {
-    searchParams: FilterProps;
-  }
+  // export interface HomeProps {
+  //   searchParams: FilterProps;
+  // }
 
-  export interface FilterProps {
-    water_quantity?: number;
-    year?: number;
-    name?: string;
-    limit?: number;
-    revoked?: boolean;
-    active?: boolean;
-  }
+  // export interface FilterProps {
+  //   water_quantity?: number;
+  //   year?: number;
+  //   name?: string;
+  //   limit?: number;
+  //   revoked?: boolean;
+  //   active?: boolean;
+  // }
